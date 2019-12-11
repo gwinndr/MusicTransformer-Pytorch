7e8df94a5787f9a971ce05abb6c9ad0bd4b6851f
gwinndr <d.gwinn2281@gmail.com>
2019-12-11 15:01:25 -0500

    Added ability to take random sequence from a midi file over the max sequence

--- dataset/e_piano.py
+++ dataset/e_piano.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
@@ -7,11 +8,14 @@ from torch.utils.data import Dataset
 from utilities.constants import *
 from utilities.tensors import create_tensor, create_full_tensor
 
+SEQUENCE_START = -1
+
 # EPianoDataset
 class EPianoDataset(Dataset):
-    def __init__(self, root, max_seq=2048):
+    def __init__(self, root, max_seq=2048, random_seq=True):
         self.root       = root
         self.max_seq    = max_seq
+        self.random_seq = random_seq
 
         fs = [os.path.join(root, f) for f in os.listdir(self.root)]
         self.data_files = [f for f in fs if os.path.isfile(f)]
@@ -41,9 +45,34 @@ class EPianoDataset(Dataset):
             x[1:raw_len+1] = raw_mid
             tgt[:raw_len]  = raw_mid
         else:
-            # TODO: Would it be better to randomly select a range?
-            x[1:]               = raw_mid[:ideal_len]
-            tgt                 = raw_mid[:self.max_seq] # Overwritting TOKEN_END
+            # Randomly selecting a range
+            if(self.random_seq):
+                end_range = raw_len - self.max_seq
+                x_start = random.randint(SEQUENCE_START, end_range)
+
+            # Always taking from the start to as far as we can
+            else:
+                x_start = SEQUENCE_START
+
+            x_end = x_start + self.max_seq
+
+            tgt_start = x_start + 1
+            tgt_end = x_end + 1
+
+            # Includes TOKEN_START
+            if(x_start == SEQUENCE_START):
+                x[1:] = raw_mid[:x_end]
+            else:
+                x = raw_mid[x_start:x_end]
+
+            # Includes TOKEN_END
+            if(tgt_end == raw_len + 1):
+                tgt[:ideal_len] = raw_mid[tgt_start:tgt_end-1]
+            else:
+                tgt = raw_mid[tgt_start:tgt_end]
+
+        # print("x:",x)
+        # print("tgt:",tgt)
 
         return x, tgt
 
