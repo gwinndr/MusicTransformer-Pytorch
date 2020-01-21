f94500a0917928e66fe3fdf0081c796a043d6a6f
gwinndr <d.gwinn2281@gmail.com>
2020-01-21 15:13:19 -0500

    Added comparisons between multiple models

--- graph_results.py
+++ graph_results.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 
 # graph_results
-def graph_results(input_folder="./saved_models", output_dir=None, epoch_start=0, epoch_end=None):
+def graph_results(input_dirs="./saved_models", output_dir=None, model_names=None, epoch_start=0, epoch_end=None):
     """
     ----------
     Author: Ben Myrick
@@ -13,34 +13,53 @@ def graph_results(input_folder="./saved_models", output_dir=None, epoch_start=0,
     ----------
     """
 
+    input_dirs = input_dirs.split(':')
+
+    if(model_names is not None):
+        model_names = model_names.split(':')
+        if(len(model_names) != len(input_dirs)):
+            print("Error: len(model_names) != len(input_dirs)")
+            return
+
     #Initialize Loss and Accuracy arrays
-    loss_arr = []
-    accuracy_arr = []
-    epochs = []
+    loss_arrs = []
+    accuracy_arrs = []
+    epoch_counts = []
 
-    fs = [os.path.join(input_folder, f) for f in sorted(os.listdir(input_folder))]
-    fs = [f for f in fs if os.path.isfile(f)]
+    for input_dir in input_dirs:
+        loss_arr = []
+        accuracy_arr = []
+        epoch_count = []
 
-    if(epoch_end is None):
-        epoch_end = len(fs)
+        fs = [os.path.join(input_dir, f) for f in sorted(os.listdir(input_dir))]
+        fs = [f for f in fs if os.path.isfile(f)]
 
-    print("Gathering results...")
-    for x in range(epoch_start, epoch_end):
-        path = fs[x]
+        if(epoch_end is None):
+            epoch_end = len(fs)
+        else:
+            epoch_end = min(epoch_end, len(fs))
 
-        #Read file and parse accuracy and loss values
-        file = open(path, 'r')
-        temp_average_accuracy = file.readline()
-        temp_average_loss = file.readline()
+        epoch_start = max(epoch_start, 0)
+        epoch_start = min(epoch_start, epoch_end)
 
-        #Update accuracy and loss arrays for each epoch
-        accuracy_arr.append(float(temp_average_accuracy))
-        loss_arr.append(float(temp_average_loss))
-        epochs.append(x)
+        for x in range(epoch_start, epoch_end):
+            path = fs[x]
 
-        file.close()
-    print("Done!")
-    print()
+            #Read file and parse accuracy and loss values
+            file = open(path, 'r')
+            temp_average_accuracy = file.readline()
+            temp_average_loss = file.readline()
+
+            #Update accuracy and loss arrays for each epoch
+            accuracy_arr.append(float(temp_average_accuracy))
+            loss_arr.append(float(temp_average_loss))
+            epoch_count.append(x)
+
+            file.close()
+
+        loss_arrs.append(loss_arr)
+        accuracy_arrs.append(accuracy_arr)
+        epoch_counts.append(epoch_count)
 
     if(output_dir is not None):
         try:
@@ -50,26 +69,45 @@ def graph_results(input_folder="./saved_models", output_dir=None, epoch_start=0,
         else:
             print ("Successfully created the directory %s" % output_dir)
 
-    #Create and save plots to output folder
-    plt.plot(epochs,loss_arr)
-    plt.title("Loss Results")
-    plt.ylabel('Loss (Cross Entropy)')
-    plt.xlabel('Epochs')
-    fig1 = plt.gcf()
-    plt.show()
+    for i in range(len(loss_arrs)):
+        if(model_names is None):
+            name = input_dirs[i]
+        else:
+            name = model_names[i]
+
+        #Create and save plots to output folder
+        plt.plot(epoch_counts[i], loss_arrs[i], label=name)
+        plt.title("Loss Results")
+        plt.ylabel('Loss (Cross Entropy)')
+        plt.xlabel('Epochs')
+        fig1 = plt.gcf()
+
+    plt.legend(loc="upper left")
 
     if(output_dir is not None):
         fig1.savefig(os.path.join(output_dir, 'loss_graph.png'))
 
-    plt.plot(epochs,accuracy_arr)
-    plt.title("Accuracy Results")
-    plt.ylabel('Accuracy (%)')
-    plt.xlabel('Epochs')
-    fig2 = plt.gcf()
     plt.show()
+
+    for i in range(len(loss_arrs)):
+        if(model_names is None):
+            name = input_dirs[i]
+        else:
+            name = model_names[i]
+
+        #Create and save plots to output folder
+        plt.plot(epoch_counts[i], accuracy_arrs[i], label=name)
+        plt.title("Accuracy Results")
+        plt.ylabel('Accuracy (%)')
+        plt.xlabel('Epochs')
+        fig2 = plt.gcf()
+
+    plt.legend(loc="upper left")
 
     if(output_dir is not None):
         fig2.savefig(os.path.join(output_dir, 'accuracy_graph.png'))
+
+    plt.show()
 
 # parse_args
 def parse_args():
@@ -83,8 +121,9 @@ def parse_args():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-input_dir", type=str, default="./saved_models/results", help="Input results folder from trained model ('results' folder)")
+    parser.add_argument("-input_dirs", type=str, default="./saved_models/results", help="Input results folder from trained model ('results' folder). Seperate with ':' for comparisons between models")
     parser.add_argument("-output_dir", type=str, default=None, help="Optional output folder to save graph pngs")
+    parser.add_argument("-model_names", type=str, default=None, help="Names to display when color coding, seperate with ':'.")
     parser.add_argument("-epoch_start", type=int, default=0, help="Epoch start. Defaults to first file.")
     parser.add_argument("-epoch_end", type=int, default=None, help="Epoch end (non-inclusive). Defaults to None.")
 
@@ -102,7 +141,7 @@ def main():
 
     args = parse_args()
 
-    graph_results(args.input_dir, args.output_dir, args.epoch_start, args.epoch_end)
+    graph_results(args.input_dirs, args.output_dir, args.model_names, args.epoch_start, args.epoch_end)
 
 if __name__ == "__main__":
     main()
