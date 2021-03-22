ee9d58dfe3fcd17d7b84cd13adbc1c7bd31cc553
Alex <asigalov61@hotmail.com>
2021-03-22 04:51:59 -0700

    Version 1.0 Initial release
    Prep for TMIDI

--- preprocess_midi.py
+++ preprocess_midi.py
@@ -1,11 +1,93 @@
+# Requirements for this updated version/TMIDI version
+
+# git clone https://github.com/asigalov61/tegridy-tools
+# Then import TMIDI module from ./tegridy-tools/tegridy-tools dir
+
+# pip install pretty_midi
+ 
+
 import argparse
 import os
 import pickle
 import json
 
-import third_party.midi_processor.processor as midi_processor
+import TMIDI
+
+import pickle
+import sys
+
+from abc import ABC, abstractmethod
+
+import pretty_midi as pyd
+from pretty_midi import Note
+
+from pprint import pprint
 
 JSON_FILE = "maestro-v2.0.0.json"
+
+'''
+This is the data processing script
+
+Courtesy of X-Labs
+https://github.com/music-x-lab/POP909-Dataset
+
+PLEASE NOTE THAT THE LICENSE FOR THIS CODE IS MIT
+
+
+============
+This script will allow you to quickly process the MIDI Files into the Google Magenta's music representation 
+    as like [Music Transformer](https://magenta.tensorflow.org/music-transformer) 
+            [Performance RNN](https://magenta.tensorflow.org/performance-rnn).
+
+'''
+
+total = 0
+def process_midi(path):
+    global total
+    data = pyd.PrettyMIDI(path)
+    main_notes = []
+    acc_notes = []
+    for ins in data.instruments:
+        acc_notes.extend(ins.notes)
+    for i in range(len(main_notes)):
+        main_notes[i].start = round(main_notes[i].start,2)
+        main_notes[i].end = round(main_notes[i].end,2)
+    for i in range(len(acc_notes)):
+        acc_notes[i].start = round(acc_notes[i].start,2)
+        acc_notes[i].end = round(acc_notes[i].end,2)
+    main_notes.sort(key = lambda x:x.start)
+    acc_notes.sort(key = lambda x:x.start)
+    mpr = TMIDI.Tegridy_RPR_MidiEventProcessor()
+    repr_seq = mpr.encode(acc_notes)
+    total += len(repr_seq)
+    print('Converted file:', path)
+    print('Total INTs count:', len(repr_seq))
+    print('=' * 70)
+    return repr_seq
+
+def process_all_midis(midi_root, save_dir):
+    save_py = []
+    midi_paths = [d for d in os.listdir(midi_root)]
+    i = 0
+    out_fmt = '{}-{}.data'
+    for path in midi_paths:
+        pprint(path)
+        filename = midi_root + path
+        try:
+            data = process_midi(filename)
+        except KeyboardInterrupt:
+            print(' Abort')
+            return
+        except EOFError:
+            print('EOF Error')
+            return
+        save_py.append(data)
+    # pprint(save_py, compact=True)    
+    save_py = np.array(save_py, dtype='object')
+    print('=' * 70)
+    print('Total number of converted MIDIs:', save_py.size)
+    print('Total INTs count:', total)
+    np.save(save_dir + 'notes_representations.npy', save_py)
 
 # prep_midi
 def prep_midi(maestro_root, output_dir):
@@ -57,7 +139,7 @@ def prep_midi(maestro_root, output_dir):
             print("ERROR: Unrecognized split type:", split_type)
             return False
 
-        prepped = midi_processor.encode_midi(mid)
+        prepped = process_midi(mid)
 
         o_stream = open(o_file, "wb")
         pickle.dump(prepped, o_stream)
