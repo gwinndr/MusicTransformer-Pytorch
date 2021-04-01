e530c42a749b43b09bbe8a92004be2022d222541
Damon R. Gwinn <d.gwinn2281@gmail.com>
2021-03-31 21:22:12 -0400

    Support for maestro V3 dataset

--- preprocess_midi.py
+++ preprocess_midi.py
@@ -5,7 +5,76 @@ import json
 
 import third_party.midi_processor.processor as midi_processor
 
-JSON_FILE = "maestro-v2.0.0.json"
+# Possible maestro filenames with the version mapping
+MAESTRO_JSON_FILENAMES = ["maestro-v3.0.0.json", "maestro-v2.0.0.json"]
+MAESTRO_VERSIONS = [3, 2]
+
+# parse_v3
+def parse_v3(maestro_json, maestro_root):
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Parses V3 maestro json into lists of train, validation, and test files
+
+    Returns 3 lists: train, val, and test in that order
+    ----------
+    """
+
+    train = []
+    val = []
+    test = []
+
+    midi_files = list(maestro_json["midi_filename"].values())
+    splits = list(maestro_json["split"].values())
+
+    for i, midi_file in enumerate(midi_files):
+        split = splits[i]
+
+        mid = os.path.join(maestro_root, midi_file)
+
+        if(split == "train"):
+            train.append(mid)
+        elif(split == "validation"):
+            val.append(mid)
+        elif(split == "test"):
+            test.append(mid)
+        else:
+            print("ERROR: Unrecognized split type:", split)
+
+    return train, val, test
+
+# parse_v2
+def parse_v2(maestro_json, maestro_root):
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Parses V2 maestro json into lists of train, validation, and test files
+
+    Returns 3 lists: train, val, and test in that order
+    ----------
+    """
+
+    train = []
+    val = []
+    test = []
+
+    for piece in maestro_json:
+        mid = os.path.join(maestro_root, piece["midi_filename"])
+        split_type = piece["split"]
+
+        if(split_type == "train"):
+            train.append(mid)
+        elif(split_type == "validation"):
+            val.append(mid)
+        elif(split_type == "test"):
+            test.append(mid)
+        else:
+            print("ERROR: Unrecognized split type:", split_type)
+
+    return train, val, test
+
 
 # prep_midi
 def prep_midi(maestro_root, output_dir):
@@ -25,51 +94,73 @@ def prep_midi(maestro_root, output_dir):
     test_dir = os.path.join(output_dir, "test")
     os.makedirs(test_dir, exist_ok=True)
 
-    maestro_json_file = os.path.join(maestro_root, JSON_FILE)
-    if(not os.path.isfile(maestro_json_file)):
-        print("ERROR: Could not find file:", maestro_json_file)
+    maestro_json_files = [os.path.join(maestro_root, json) for json in MAESTRO_JSON_FILENAMES]
+
+    # Set maestro json depending on which version is found
+    maestro_json_file = None
+    maestro_version = -1
+    for i, json_candidate in enumerate(maestro_json_files):
+        if(os.path.isfile(json_candidate)):
+            maestro_json_file = json_candidate
+            maestro_version = MAESTRO_VERSIONS[i]
+            print("Found file:", maestro_json_file)
+            print("Maestro version:", maestro_version)
+            break
+
+    # Error if no maestro json found
+    if(maestro_json_file is None):
+        print("ERROR: Could not find maestro json file!")
+        print("Possible maestro json names (in maestro_root):", MAESTRO_JSON_FILENAMES)
         return False
 
     maestro_json = json.load(open(maestro_json_file, "r"))
-    print("Found", len(maestro_json), "pieces")
-    print("Preprocessing...")
 
-    total_count = 0
-    train_count = 0
-    val_count   = 0
-    test_count  = 0
+    # Parse json into train, val, and test files based on version
+    if(maestro_version == 3):
+        train, val, test = parse_v3(maestro_json, maestro_root)
+    elif(maestro_version == 2):
+        train, val, test = parse_v2(maestro_json, maestro_root)
+    else:
+        print("BUG: No parser for version:", maestro_version)
+        return False
 
-    for piece in maestro_json:
-        mid         = os.path.join(maestro_root, piece["midi_filename"])
-        split_type  = piece["split"]
-        f_name      = mid.split("/")[-1] + ".pickle"
+    num_train = len(train)
+    num_val = len(val)
+    num_test = len(test)
 
-        if(split_type == "train"):
-            o_file = os.path.join(train_dir, f_name)
-            train_count += 1
-        elif(split_type == "validation"):
-            o_file = os.path.join(val_dir, f_name)
-            val_count += 1
-        elif(split_type == "test"):
-            o_file = os.path.join(test_dir, f_name)
-            test_count += 1
-        else:
-            print("ERROR: Unrecognized split type:", split_type)
-            return False
+    print("Found", num_train, "train pieces")
+    print("Found", num_val, "val pieces")
+    print("Found", num_test, "test pieces")
 
-        prepped = midi_processor.encode_midi(mid)
+    all_pieces = ( ("train", train), ("validation", val), ("test", test) )
+    for piece in all_pieces:
+        split_type = piece[0]
+        mids = piece[1]
+        total_count = 0
 
-        o_stream = open(o_file, "wb")
-        pickle.dump(prepped, o_stream)
-        o_stream.close()
+        print("Prepping %s..." % split_type)
+        for mid in mids:
+            o_name = mid.split("/")[-1] + ".pickle"
 
-        total_count += 1
-        if(total_count % 50 == 0):
-            print(total_count, "/", len(maestro_json))
+            if(split_type == "train"):
+                o_file = os.path.join(train_dir, o_name)
+            elif(split_type == "validation"):
+                o_file = os.path.join(val_dir, o_name)
+            elif(split_type == "test"):
+                o_file = os.path.join(test_dir, o_name)
+            else:
+                print("ERROR: Unrecognized split type:", split_type)
 
-    print("Num Train:", train_count)
-    print("Num Val:", val_count)
-    print("Num Test:", test_count)
+            prepped = midi_processor.encode_midi(mid)
+
+            o_stream = open(o_file, "wb")
+            pickle.dump(prepped, o_stream)
+            o_stream.close()
+
+            total_count += 1
+            if(total_count % 50 == 0):
+                print(total_count, "/", len(mids), split_type)
+
     return True
 
 
@@ -87,7 +178,9 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("maestro_root", type=str, help="Root folder for the Maestro dataset")
-    parser.add_argument("-output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into")
+    parser.add_argument("-output_dir", type=str, default="./dataset/e_piano",
+        help="Output folder to put the preprocessed midi into (recommend you leave at default)")
+
 
     return parser.parse_args()
 
